956b6559702764c78f98f0bd751d523182b8f977
suthyscott <sutherlandscott21@gmail.com>
2023-08-17 10:30:54 -0600

    added comments explaing new syntax

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 import express from 'express' 
 import session from 'express-session'
+// Here we are importing the default export from the authController.js file. This will be an object with all the functions we've declared (as methods in an object).
 import ctrl from './controllers/authController.js'
+
 const PORT = 4545
 
 const app = express()
 
+// To 'parse' in code essentially means to translate from one language or format to another. In the below examples, we're parsing an object from the 'urlencoded' format or the JSON format into a JavaScript object. 
+
+// This parses body objects that come directly from an html form, which sends them in a format called "urlencoded". You can also send body objects using this format manually from Postman, as seen in the 'form-encoded.png' file in this repo. 
 app.use(express.urlencoded({extended: false}))
-// app.use(express.json())
+
+// This parses body objects that are formatted as raw json. We will be sending requests with JSON objects soon, but you can do this manually with Postman as seen in the image file called 'raw-json.png' in this repo. 
+app.use(express.json())
+
 app.use(session({
     resave: false,
     saveUninitialized: true,
     secret: 'asdf;alsdkjfas;ldgkj'
 }))
 
+// destructuring the handler functions we need from the object we're exporting from the authController. 
 const {handleSeeMe, handleBaseEndpoint, handleAddMeToSess} = ctrl
 
-
+// Instead of writing handler functions right here, we use the functions we destructured from the controller above. These are callback functions we're passing in, so they should not be invoked. 
 app.get('/', handleBaseEndpoint)
 
 app.get('/seeme', handleSeeMe)
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,18 +1,23 @@
+// we are exporting an object with three properties. The values for each of the properties are anonymous arrow functions. Because these functions will be invoked by the endpoints, they can receive req and res as parameters. 
 export default {
-    handleBaseEndpoint: (req, res) => {
-        console.log('hit', req.session)
-        const {name, age, favColor} = req.session.me
-        res.send(`<h1>Hello there ${name}, ${age} ${favColor}</h1>`)
-    },
     handleSeeMe: (req, res) => {
+        // we can received queries from Postman just like a request from the browser.
         const {name} = req.query
         res.send(`Hi there ${name}`)
     },
     handleAddMeToSess: (req,res) => {
+        // receving data about the user from the req.body oject and destructuring it. 
         const {name, age, favColor} = req.body
-        console.log(name, age, favColor)
+        
+        // we then access the session object that we created on req using the express-session library, and add a new property called 'me'. The value of req.session.me is a new object with the three properties from the body.
         req.session.me = {name, age, favColor}
-        console.log(req.session)
+        
+        // we're not trying to send back any data from this function, so we just send back a confirmation code (200 means OK) to tell the user that their post request worked. 
         res.sendStatus(200)
-    }
+    },
+    handleBaseEndpoint: (req, res) => {
+        // as long as we've used the post request above to add a user's data to the session, we can destructure those session properties and use them however we want, such as sending back some HTML with that information. 
+        const {name, age, favColor} = req.session.me
+        res.send(`<h1>Hello there ${name}, ${age} ${favColor}</h1>`)
+    },
 }
